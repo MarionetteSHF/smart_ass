18c265deb286b866aae282386229916b55caf789
Jialin1210 <lynnzhu1210@gmail.com>
2022-10-28 17:48:25 -0400

    test user spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,32 @@
 class UsersController < ApplicationController
+    private
+    # Making "internal" methods private is not required, but is a common practice.
+    # This helps make clear which methods respond to requests, and which ones do not.
+    def user_params
+      params.require(:user).permit(:name, :email, :phone, :encrypted_password)
+    end
+
+    def new
+        @user = User.new
+    end
+    
     def index
         @users = User.all
+    end
+
+    def create
+        puts params
+        user_params[:user_id] = User.set_user_id
+        
+        @user = User.create!(user_params)
+        if @user.save
+            flash[:notice] = "#{@user.name} was successfully created"
+            redirect_to user_path
+        end
+    end
+
+    def login
+
     end
 
 end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,11 @@
+# require 'rails_helper'
+
+# RSpec.describe UsersController do
+#     describe 'new user' do
+#         it 'should be able to create a user account' do
+#             post :create, user:{'name': 'abc', 'phone': '12345678', 'email': 'abd@gmail.com', 'encrypted_password': 'abcabc'}
+#             expect(notice).to match(/abc was successfully created/)
+#             expect(response).to redirect_to(user_path)
+#         end
+#     end
+# end
