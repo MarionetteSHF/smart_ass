bc350057620313cc482dc7694188bffa493e15fd
wuw-github <wuw1998@gmail.com>
2022-10-30 15:18:38 -0400

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_28_212825) do
     t.datetime "updated_at", null: false
   end
 
-  
-
   create_table "users", force: :cascade do |t|
     t.string "phone"
     t.string "name"
