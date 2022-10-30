fa63c46ffe189384c292232f5485c45700f131d1
Hanfu Shi <hanfu.shi2019@gmail.com>
2022-10-30 15:02:37 -0400

    delete movie related files

--- db/schema.rb
+++ db/schema.rb
@@ -22,14 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_28_212825) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.string "rating"
-    t.text "description"
-    t.datetime "release_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  
 
   create_table "users", force: :cascade do |t|
     t.string "phone"
