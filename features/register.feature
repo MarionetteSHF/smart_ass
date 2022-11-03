eba75a2c9d0353d7d24a488f4f27b3d6c3f6552a
Jialin1210 <lynnzhu1210@gmail.com>
2022-11-02 20:51:28 -0400

    improve user cucumber coverage

--- features/register.feature
+++ features/register.feature
@@ -7,6 +7,12 @@ Scenario: Error on empty fields
     When I press "Register"
     Then I should see "please fill in all information"
 
+Scenario: Error on missing fields
+    When I fill in "Email address" with "test@gmail.com"
+    And I fill in "Phone Number" with "12345"
+    Then I press "Register"
+    And I should see "please fill in all information"
+    
 Scenario: Register successfully
     When I fill in "Name" with "abc"
     And I fill in "Email address" with "abc@gmail.com"
--- features/step_definitions/users_steps.rb
+++ features/step_definitions/users_steps.rb
@@ -3,21 +3,4 @@ Given /the following users exist/ do |users|
       User.create user
     end
   end
-  Then /I should see all the users/ do
-    User.all.each do |user|
-      step %{I should see "#{user.name}"}
-    end
-  end
 
-Given /^I am an authenticated user$/ do
-    name = 'abc'
-    email = 'abc@gmail.com'
-    phone = '123456'
-    password = 'abcabc'
-
-    Given %{I have one user "#{name}" with email "#{email}"and phone"#{phone}" and password "#{password}"}
-    And %{I go to the user login page}
-    And %{I fill in "email" with "#{email}"}
-    And %{I fill in "password" with "#{password}"}
-    And %{I press "Login"}
-end
