332d9a94814762c4ef2a556057c3d3a46a5460d5
wuw-github <wuw1998@gmail.com>
2022-11-02 20:19:21 -0400

    add

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,6 +32,7 @@ class ItemsController < ApplicationController
       flash[:notice] = "#{@item.title} was successfully created"
       redirect_to item_path(@item)
     else
+      flash[:notice] = @item.errors.full_messages
       render 'new', :status=>422
     end
   end
