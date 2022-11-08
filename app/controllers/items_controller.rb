f954f65e58771fda39c373761d049d72be1e7658
wuw-github <wuw1998@gmail.com>
2022-11-07 22:32:15 -0500

    finish images upload

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -57,6 +57,7 @@ class ItemsController < ApplicationController
   # PATCH/PUT /items/1 or /items/1.json
   def update
     @item = Item.find params[:id]
+    @item.images.purge
     if @item.update(item_params)
       flash[:notice] = "#{@item.title} was successfully updated."
       redirect_to item_path(@item)
@@ -69,6 +70,7 @@ class ItemsController < ApplicationController
   # DELETE /items/1 or /items/1.json
   def destroy
     @item = Item.find_by_id params[:id]
+    @item.images.purge
     @item.destroy
     flash[:notice] = "Item '#{@item.title}' has been deleted"
     redirect_to items_path
